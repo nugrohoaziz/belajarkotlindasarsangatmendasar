f4e79468cec1587666cfe75b1d02a2444e355911
nugrohoaziz <azizagusn17@gmail.com>
2022-04-20 12:54:22 +0700

    belajar number value

---
+++ src/main/kotlin/Number.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val age: Byte = 30
+    val heig: Int = 170
+    val distance: Short = 2000
+    val balance: Long = 100000000L
+
+    println(age)
+    println(heig)
+    println(distance)
+    println(balance)
+}
