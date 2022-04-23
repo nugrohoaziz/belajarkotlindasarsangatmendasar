ccd004834f78302ee29445ff36d92dc687ae8bbf
nugrohoaziz <azizagusn17@gmail.com>
2022-04-23 15:15:53 +0700

    Learn String 1

---
+++ src/main/kotlin/String.kt
@@ -0,0 +1,25 @@
+fun main() {
+    var namaawal: String = "aziz"
+    var namatengah: String = "nugroho"
+    var bio: String = """ 
+saya adalah aziz
+aziz adalaha saya
+siapa saya?
+        """
+    println(bio)
+
+    //menggunakan pipe yaitu "|"
+    var alamat: String ="""
+        |b
+        |Cilegon
+        |no where
+    """.trimMargin()
+    println(alamat)
+
+    //menggunakan character yaitu ">"
+    var lahir: String = """
+        >univers 2.0
+        >mars
+        >lintang 45
+    """.trimMargin(">")
+}
