75cf2eb28c2038757af00a0ef5c9404990955415
nugrohoaziz <azizagusn17@gmail.com>
2022-04-24 17:00:19 +0700

    learn math operator 2 augmented asigment

--- src/main/kotlin/MathOperator.kt
+++ src/main/kotlin/MathOperator.kt
@@ -9,5 +9,23 @@ fun main() {
     println(result3)
     println(result3.toDouble())
 
+    //add up all prices
+    var total = 0
+    val barang1 = 100
+    total = total + barang1
+    val barang2 = 200
+    total = total + barang2
+    val barang3 = 300
+    total = total + barang3
+    println(total)
 
+    //augmanted asigment
+    var total2 = 0
+    val phone1 = 100
+    total2 += phone1
+    val phone2 = 200
+    total2 += phone2
+    val phone3 = 300
+    total2 += phone3
+    println(total2)
 }
