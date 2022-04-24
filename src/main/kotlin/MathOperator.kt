57edebffac13db2363e22f2227c06c24cc306149
nugrohoaziz <azizagusn17@gmail.com>
2022-04-24 16:51:25 +0700

    learn math operator 1

---
+++ src/main/kotlin/MathOperator.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val result1: Double = 8.3 / 3
+    println(result1.toInt())
+
+    val result2: Double = 10 + 78.0 / 4
+    println(result2)
+
+    val result3: Int = 10 + 78 / 4
+    println(result3)
+    println(result3.toDouble())
+
+
+}
