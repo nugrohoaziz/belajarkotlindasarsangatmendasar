d190212d80521c474d77d8389b3e49519b4b080a
nugrohoaziz <azizagusn17@gmail.com>
2022-04-27 00:36:50 +0700

    mingguan test mandiri 3

--- src/main/kotlin/HarianTestMandiri.kt
+++ src/main/kotlin/HarianTestMandiri.kt
@@ -7,16 +7,31 @@ fun main() {
     val kecepatanStabil = 5..10
     val hasilKecepan = kecepatanStabil.random()
     var kecepatanJalan = 2
-    if(kecepatanJalan >= kecepatanMinimal && kecepatanJalan <= kecepatanMaximal ){
-       println("kecepatan stabil")
-    } else {
-        when(warningCode){
-            "w" -> {
-                kecepatanJalan = hasilKecepan
-                println("Kecepan berubah menjadi stabil yaitu $kecepatanJalan")
-            }
-            "x" -> println("kecepatan tidak stabil")
+    //if(kecepatanJalan >= kecepatanMinimal && kecepatanJalan <= kecepatanMaximal ){
+       //println("kecepatan stabil")
+    //} else {
+        //when(warningCode){
+            //"w" -> {
+                //kecepatanJalan = hasilKecepan
+                //println("Kecepan berubah menjadi stabil yaitu $kecepatanJalan")
+            //}
+            //"x" -> println("kecepatan tidak stabil")
+        //}
+    //}
+
+    when{
+        kecepatanJalan >= kecepatanMinimal && kecepatanJalan <= kecepatanMaximal -> {
+            println("kecpatan stabil")
+        } else -> {
+            kecepatanJalan = hasilKecepan
+            println("kecepatan stabil yaitu $kecepatanJalan")
         }
+    }
+
+    val beratManusia = arrayOf(29, 38, 47, 56)
+    val tinggiManusia = arrayOf(30,40,50,60)
+    if (tinggiManusia[0] <= 30 && beratManusia[0] < tinggiManusia[0]){
+        println("manusia ideal")
     }
 
 
--- src/main/kotlin/WhenExpression.kt
+++ src/main/kotlin/WhenExpression.kt
@@ -1,3 +1,15 @@
 fun main() {
+    val nilai = "D"
+    val kecepatan = 10
 
+    when(nilai){
+        "A", "B" -> println("kamu lulus")
+        else -> println("kamu ga lulus")
+    }
+
+    val kecepatanJalan = arrayOf(5,6,7,8)
+    when(kecepatan){
+        in kecepatanJalan -> println("kecepatan stabil")
+        !in kecepatanJalan -> println("kecepan tidak stabil")
+    }
 }
