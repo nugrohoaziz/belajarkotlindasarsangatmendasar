9f4557d20c599d0f9b5dbb73dd762d6b72f30fad
nugrohoaziz <azizagusn17@gmail.com>
2022-04-26 13:10:32 +0700

    mingguan test mandiri 2

--- src/main/kotlin/HarianTestMandiri.kt
+++ src/main/kotlin/HarianTestMandiri.kt
@@ -3,16 +3,19 @@ import jdk.incubator.vector.VectorOperators.Binary
 fun main() {
     val kecepatanMinimal = 5
     val kecepatanMaximal = 10
-    val warningCode = true
+    val warningCode = "w"
     val kecepatanStabil = 5..10
     val hasilKecepan = kecepatanStabil.random()
     var kecepatanJalan = 2
     if(kecepatanJalan >= kecepatanMinimal && kecepatanJalan <= kecepatanMaximal ){
        println("kecepatan stabil")
     } else {
-        if (warningCode){
-            kecepatanJalan = hasilKecepan
-            println("kecepatan menjadi stabil yaitu ${kecepatanJalan}")
+        when(warningCode){
+            "w" -> {
+                kecepatanJalan = hasilKecepan
+                println("Kecepan berubah menjadi stabil yaitu $kecepatanJalan")
+            }
+            "x" -> println("kecepatan tidak stabil")
         }
     }
 
