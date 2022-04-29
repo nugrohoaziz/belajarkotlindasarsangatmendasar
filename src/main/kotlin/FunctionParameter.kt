e14d5a27daa859d97eaa264a0103c62ec057cc13
nugrohoaziz <azizagusn17@gmail.com>
2022-04-29 21:24:29 +0700

    learn function parameter 2

--- src/main/kotlin/FunctionParameter.kt
+++ src/main/kotlin/FunctionParameter.kt
@@ -1,7 +1,17 @@
 fun main() {
     sayHello("Hurin", "Vina")
+    sayName("aziz", null)
+    sayName("Hurin'in", "Firdaus")
 }
 
 fun sayHello(teman1: String, teman2: String){
     println("hello $teman1 $teman2")
+}
+
+fun sayName(firstName:String, lastName:String?){
+    if (lastName == null){
+        println("hallow $firstName")
+    }else{
+        println("hellow $firstName $lastName")
+    }
 }
