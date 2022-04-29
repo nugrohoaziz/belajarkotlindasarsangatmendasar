b02df8c57891fbc7388d45c4863a6a7c33b6314b
nugrohoaziz <azizagusn17@gmail.com>
2022-04-29 17:05:58 +0700

    learn while loops 1

---
+++ src/main/kotlin/WhileLoops.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var lap = 0
+
+    //3. cek apakah nilai lap di bahaw(lebih kecil) 10
+    while (lap < 10){
+        //1. menjalankan print lap dahulu
+        println(lap)
+        //2. lalu di tambah nilai 1
+        lap++
+    }
+}
