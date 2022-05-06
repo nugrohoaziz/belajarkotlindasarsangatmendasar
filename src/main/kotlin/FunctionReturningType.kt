253ffe1efee971f072718b5ecc0016c732f82204
nugrohoaziz <azizagusn17@gmail.com>
2022-05-06 23:04:01 +0700

    learn Function Returning Type 2 with streing

--- src/main/kotlin/FunctionReturningType.kt
+++ src/main/kotlin/FunctionReturningType.kt
@@ -1,11 +1,18 @@
 fun main() {
     val hasil = penjulahanUang(0,90,10)
+    val buku = stringlist(namaBuku = "Kisah Sukses Aziz", namaPengarang = "Aziz")
     println("total jumlah pembelian makanan hari selasa adalah $hasil")
+    println("semua manusia dibumi selama 90 tahun membeli $buku")
 }
 
 
 fun penjulahanUang (uangAziz: Int, uangHabib: Int, uangAdam: Int): Int{
     val perhitungan = uangAziz + uangHabib + uangAdam
     return perhitungan
+}
+
+fun stringlist(namaPengarang: String, namaBuku: String):String{
+    val aaraayBook = "$namaPengarang, $namaBuku"
+    return aaraayBook
 }
 
