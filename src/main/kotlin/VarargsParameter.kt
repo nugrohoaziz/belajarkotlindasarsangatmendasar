862736602e26f7d4c9335a5c407459ded9474431
nugrohoaziz <azizagusn17@gmail.com>
2022-05-12 23:28:23 +0700

    learn Varargs PArameter part 1

---
+++ src/main/kotlin/VarargsParameter.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val lupi = arrayOf("baeishihdf", "bvudhsfkljhd")
+    val result = mencobaMengembalikanArray(lupi)
+    println("${result.toList()}")
+
+}
+
+fun mencobaMengembalikanArray(lapi: Array<String>): Array<String> {
+    return lapi
+}
+
