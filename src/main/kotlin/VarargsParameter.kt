f058b3591fabfdad18918af1f126488082484e51
nugrohoaziz <azizagusn17@gmail.com>
2022-05-14 22:32:51 +0700

    learn Varargs PArameter part 2 peraktek langsung varargsParameternya

--- src/main/kotlin/VarargsParameter.kt
+++ src/main/kotlin/VarargsParameter.kt
@@ -3,9 +3,20 @@ fun main() {
     val result = mencobaMengembalikanArray(lupi)
     println("${result.toList()}")
 
+    val seribuPukulan = lufiSiBajakLaut(10,10,10,10)
+    println("jurus seribu tonjokan $seribuPukulan")
+
 }
 
 fun mencobaMengembalikanArray(lapi: Array<String>): Array<String> {
     return lapi
+}
+
+fun lufiSiBajakLaut(vararg banyaks: Int): Int{
+    var tangan = 1
+    for (banyak in banyaks){
+        tangan += banyak
+    }
+    return tangan
 }
 
